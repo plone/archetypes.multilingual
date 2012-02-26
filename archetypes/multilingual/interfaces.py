4bc0ab83acf06a6829acaabfccddef01d3bcf69e
JC Brand <jc@opkode.com>
2012-02-26 21:46:05 +0200

    Implement the ILanguageIndependentFieldsManager

--- archetypes/multilingual/interfaces.py
+++ archetypes/multilingual/interfaces.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-# vim: set ts=4 sw=4:
 from plone.multilingual.interfaces import ITranslatable
-
 
 class IArchetypesTranslatable(ITranslatable):
     """ translatable-marker for archetypes """
--- archetypes/multilingual/utils.py
+++ archetypes/multilingual/utils.py
@@ -6,10 +6,26 @@ from plone.multilingual.interfaces import ILanguageIndependentFieldsManager
 class LanguageIndependentFieldsManager(object):
     interface.implements(ILanguageIndependentFieldsManager)
 
-    def get_field_names(self):
-        # TODO
-        return
+    def _copy_field(self, field, translation):
+        accessor = field.getEditAccessor(self.context)
+        if not accessor:
+            accessor = field.getAccessor(self.context)
+        if accessor:
+            data = accessor()
+        else:
+            data = field.get(self.context)
+        mutator = field.getMutator(translation)
+        if mutator is not None:
+            # Protect against weird fields, like computed fields
+            mutator(data)
+        else:
+            field.set(translation, data)
 
     def copy_fields(self, translation):
-        # TODO
-        return
+        source_schema = self.context.Schema()
+        dest_schema = translation.Schema()
+        schema = self.context.Schema()
+        fields = schema.filterFields(languageIndependent=True)
+        fields_to_copy = [x for x in fields if x.getName() in dest_schema]
+        for field in fields_to_copy:
+            self._copy_field(field, translation)
