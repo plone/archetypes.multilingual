5eacd6f8fb6988d967538964651de593d47edea5
Victor Fernandez de Alba <sneridagh@gmail.com>
2012-10-01 16:42:41 +0200

    Move set default language monkey patch to subscriber. Correct minor bug.

--- archetypes/multilingual/monkey.py
+++ archetypes/multilingual/monkey.py
@@ -1,26 +1,4 @@
 # -*- coding: utf-8 -*-
-from Acquisition import aq_parent
-from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone.interfaces import IPloneSiteRoot
-from plone.multilingual.interfaces import ILanguage, ITranslatable
-
-
-def default_language(self):
-    """
-        Monkey patch to control the default language from
-        new created ATs. It must default to parent's language
-        if parent not implements IPloneSiteRoot.
-    """
-    parent = aq_parent(self)
-    language_tool = getToolByName(self, 'portal_languages')
-    if language_tool.startNeutral():
-        # We leave this untouched by now.
-        language = u""
-    elif IPloneSiteRoot.implementedBy(parent):
-        language = language_tool.getPreferredLanguage()
-    elif ITranslatable.providedBy(parent):
-        language = ILanguage(parent).get_language()
-    return language
 
 
 def isVisible(self, instance, mode='view'):
--- archetypes/multilingual/subscriber.py
+++ archetypes/multilingual/subscriber.py
@@ -1,3 +1,5 @@
+from Acquisition import aq_parent
+from zope.component.hooks import getSite
 from zope.component import getUtility
 from zope.component import queryAdapter
 from zope.event import notify
@@ -5,14 +7,19 @@ from zope.lifecycleevent import Attributes
 from zope.lifecycleevent import ObjectModifiedEvent
 from zope.lifecycleevent.interfaces import IObjectModifiedEvent
 
+from Products.CMFCore.utils import getToolByName
+
 from plone.dexterity.interfaces import IDexterityFTI
 
 # from plone.multilingualbehavior.interfaces import IDexterityTranslatable
 from archetypes.multilingual.interfaces import IArchetypesTranslatable
 
 from plone.multilingual.interfaces import ILanguage
+from plone.multilingual.interfaces import ITranslatable
 from plone.multilingual.interfaces import ILanguageIndependentFieldsManager
 from plone.multilingual.interfaces import ITranslationManager
+
+from Products.CMFPlone.interfaces import IPloneSiteRoot
 
 
 class LanguageIndependentModifier(object):
@@ -65,3 +72,28 @@ class LanguageIndependentModifier(object):
         return translations_list_to_process
 
 handler = LanguageIndependentModifier()
+
+
+def set_default_language(obj, event):
+    """
+        Subscriber for controlling the default language from
+        new created ATs. It must default to parent's language
+        if parent not implements IPloneSiteRoot. Take care also
+        that the content is still at the portal_factory.
+    """
+    portal = getSite()
+    parent = aq_parent(obj)
+    language_tool = getToolByName(portal, 'portal_languages')
+
+    if language_tool.startNeutral():
+        # We leave this untouched by now.
+        language = u""
+    elif IPloneSiteRoot.providedBy(parent):
+        language = language_tool.getPreferredLanguage()
+    elif ITranslatable.providedBy(parent):
+        language = ILanguage(parent).get_language()
+    else:
+        # The parent is still the portal_factory so leave it alone
+        language = u''
+
+    ILanguage(obj).set_language(language)
