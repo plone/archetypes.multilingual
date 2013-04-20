7f9566b31e19f98319572f8af029322809c8aa77
Victor Fernandez de Alba <sneridagh@gmail.com>
2013-04-20 22:30:04 +0200

    Add session support to testing infrastructure

--- archetypes/multilingual/tests/_testing.py
+++ archetypes/multilingual/tests/_testing.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 # vim: set ts=4 sw=4:
 import doctest
+import transaction
+from OFS.Folder import Folder
+from Testing import ZopeTestCase as ztc
+
 from plone.app.testing import (
     PLONE_FIXTURE,
     PloneSandboxLayer,
@@ -15,17 +19,30 @@ from plone.testing import z2
 class ArchetypesMultilingualLayer(PloneSandboxLayer):
     defaultBases = (PLONE_FIXTURE,)
 
+    class Session(dict):
+        def set(self, key, value):
+            self[key] = value
+
     def setUpZope(self, app, configurationContext):
         # load ZCML
         import archetypes.multilingual
         import archetypes.testcase
 
         xmlconfig.file('testing.zcml', archetypes.multilingual,
-                        context=configurationContext)
+                       context=configurationContext)
         xmlconfig.file('configure.zcml', archetypes.testcase,
-                        context=configurationContext)
+                       context=configurationContext)
 
         z2.installProduct(app, 'archetypes.testcase')
+
+        # Support sessionstorage in tests
+        app.REQUEST['SESSION'] = self.Session()
+        if not hasattr(app, 'temp_folder'):
+            tf = Folder('temp_folder')
+            app._setObject('temp_folder', tf)
+            transaction.commit()
+
+        ztc.utils.setupCoreSessions(app)
 
     def setUpPloneSite(self, portal):
         # install into the Plone site
@@ -34,11 +51,11 @@ class ArchetypesMultilingualLayer(PloneSandboxLayer):
 
 ARCHETYPESMULTILINGUAL_FIXTURE = ArchetypesMultilingualLayer()
 
-ARCHETYPESMULTILINGUAL_INTEGRATION_TESTING = IntegrationTesting(\
-    bases=(ARCHETYPESMULTILINGUAL_FIXTURE,),\
+ARCHETYPESMULTILINGUAL_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(ARCHETYPESMULTILINGUAL_FIXTURE,),
     name="archetypes.multilingual:Integration")
-ARCHETYPESMULTILINGUAL_FUNCTIONAL_TESTING = FunctionalTesting(\
-    bases=(ARCHETYPESMULTILINGUAL_FIXTURE,),\
+ARCHETYPESMULTILINGUAL_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(ARCHETYPESMULTILINGUAL_FIXTURE,),
     name="archetypes.multilingual:Functional")
 
 optionflags = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
