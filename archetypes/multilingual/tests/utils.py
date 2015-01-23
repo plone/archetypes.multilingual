294f0f9c2c8f35eb289f6d44b82fbf769e503247
Daniel Widerin <daniel.widerin@zumtobelgroup.com>
2015-01-23 15:49:22 +0100

    we need _createObjectByType here
    because invokeFactory looks for global_allow property

--- archetypes/multilingual/tests/utils.py
+++ archetypes/multilingual/tests/utils.py
@@ -1,13 +1,16 @@
+# -*- coding: utf-8 -*-
+from Products.CMFPlone.utils import _createObjectByType
 from plone.app.multilingual.interfaces import ITranslationManager
 
 
-def makeContent(context, portal_type, id='doc', **kw):
-    context.invokeFactory(portal_type, id, **kw)
-    return getattr(context, id)
+def makeContent(context, type_name, id='doc', **kwargs):
+    _createObjectByType(type_name, context, id=id, **kwargs)
+    obj = context[id]
+    obj.reindexObject()
+    return obj
 
 
 def makeTranslation(content, language='en'):
     manager = ITranslationManager(content)
     manager.add_translation(language)
-    import transaction; transaction.commit()
     return manager.get_translation(language)
