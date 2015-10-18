e0dec504afbd66c6047b32575f3001dd1e35b7aa
Benoît Suttor <benoit.suttor@imio.be>
2015-10-18 10:59:46 +0300

    Use viewlet inherited from PAM

--- archetypes/multilingual/browser/viewlets.py
+++ archetypes/multilingual/browser/viewlets.py
@@ -4,37 +4,19 @@ from Products.CMFCore.utils import getToolByName
 from plone.app.layout.viewlets.common import ViewletBase
 from plone.app.multilingual.interfaces import ILanguage
 from plone.app.multilingual.interfaces import ITranslatable
+from plone.app.multilingual.browser.viewlets import AddFormIsATranslationViewlet
+from archetypes.multilingual import logger
 
 
-class addFormATIsATranslationViewlet(ViewletBase):
+class addFormATIsATranslationViewlet(AddFormIsATranslationViewlet):
     """ Notice the user that this add form is a translation
     """
     available = False
 
-    def language(self):
-        return self.lang
-
-    def origin(self):
-        return self.origin
-
-    def render(self):
-        if self.available:
-            return self.index()
-        return u""
-
     def update(self):
         """ It's only for AT on factory so we check """
-        if self.context.portal_factory.isTemporary(self.context):
-            sdm = self.context.session_data_manager
-            session = sdm.getSessionData(create=True)
-            if ITranslatable.providedBy(self.context):
-                self.lang = ILanguage(self.context).get_language()
-            else:
-                self.lang = 'NaN'
-            if 'tg' in session.keys():
-                tg = session['tg']
-                self.available = True
-                ptool = getToolByName(self.context, 'portal_catalog')
-                query = {'TranslationGroup': tg, 'Language': 'all'}
-                results = ptool.searchResults(query)
-                self.origin = results
+        # logger.info('Viewlets update, tg: {}'.format(self.request.translation_info['tg']))
+        factory = getToolByName(self.context, 'portal_factory', None)
+        if factory is None or not factory.isTemporary(self.context):
+            return
+        super(AddFormIsATranslationViewlet, self).update()
