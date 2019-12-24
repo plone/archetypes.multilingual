ed24c77b10eeddf2ad116b2e5fb5eba19f543423
Maurits van Rees <maurits@vanrees.org>
2019-12-24 14:58:36 +0100

    Normalize package data, like setup.py.
    Make pyroma and check-manifest happy.
    Use .rst instead of .txt files.

--- README.rst
+++ README.rst
@@ -3,6 +3,8 @@ Introduction
 
 This egg adds plone.app.multilingual support to Archetypes.
 
+This version is for Plone 4 only.
+
 For more information, please visit:
 
     https://github.com/plone/plone.app.multilingual
--- setup.py
+++ setup.py
@@ -1,47 +1,42 @@
 # -*- coding: utf-8 -*-
 from setuptools import find_packages
 from setuptools import setup
-import os
 
-version = '2.1.1.dev0'
+
+version = "2.1.1.dev0"
 
 setup(
-    name='archetypes.multilingual',
+    name="archetypes.multilingual",
     version=version,
-    description='Multilingual support for archetypes.',
-    long_description=u'\n'.join([
-        open('README.txt').read(),
-        open(os.path.join('docs', 'HISTORY.txt')).read(),
-    ]),
+    description="Multilingual support for archetypes.",
+    long_description=open("README.rst").read() + "\n" + open("CHANGES.rst").read(),
     # Get more strings from https://pypi.org/classifiers/
     classifiers=[
-        'Framework :: Plone',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
+        "Development Status :: 6 - Mature",
+        "Framework :: Plone",
+        "Framework :: Plone :: Addon",
+        "Framework :: Plone :: 4.3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
     ],
-    keywords='archetypes multilingual i18n translation',
-    author='awello',
-    author_email='awello@gmail.com',
-    url='https://github.com/plone/archetypes.multilingual',
-    license='GPL',
-    packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['archetypes'],
+    keywords="archetypes multilingual i18n translation",
+    author="awello",
+    author_email="awello@gmail.com",
+    url="https://github.com/plone/archetypes.multilingual",
+    license="GPL",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["archetypes"],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'setuptools',
-        'Products.ATContentTypes',
-        'plone.app.multilingual >= 2.0.4, < 3.0',
-        'collective.monkeypatcher',
-        'plone.api',
+        "setuptools",
+        "Products.ATContentTypes",
+        "plone.app.multilingual >= 2.0.4, < 3.0",
+        "collective.monkeypatcher",
+        "plone.api",
     ],
-    extras_require={
-        'test': [
-            'plone.app.testing',
-            'plone.app.multilingual[test]',
-        ]
-    },
+    extras_require={"test": ["plone.app.testing", "plone.app.multilingual[test]",]},
     entry_points="""
     [z3c.autoinclude.plugin]
     target = plone
